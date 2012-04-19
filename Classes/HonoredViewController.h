86ebf5525b1c4605117eb722e979db6d5dd02fde
Lex Tang <lexrus@gmail.com>
2012-04-19 17:31:45 +0800

    肯定要有一个HonoredViewController.

---
+++ Classes/HonoredViewController.h
@@ -0,0 +1,13 @@
+//
+//  HonoredViewController.h
+//  HonoredViewController
+//
+//  Created by Tang Lex on 4/19/12.
+//  Copyright (c) 2012 LexTang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HonoredViewController : UIViewController
+
+@end
---
+++ Classes/HonoredViewController.m
@@ -0,0 +1,44 @@
+//
+//  HonoredViewController.m
+//  HonoredViewController
+//
+//  Created by Tang Lex on 4/19/12.
+//  Copyright (c) 2012 LexTang.com. All rights reserved.
+//
+
+#import "HonoredViewController.h"
+
+@interface HonoredViewController ()
+
+@end
+
+@implementation HonoredViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
