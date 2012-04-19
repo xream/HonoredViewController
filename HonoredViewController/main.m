d6e200100fd228bea7be37cee70a35486864a1db
Lex Tang <lexrus@gmail.com>
2012-04-19 17:23:16 +0800

    项目建好啦。

---
+++ HonoredViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HonoredViewController
+//
+//  Created by Tang Lex on 4/19/12.
+//  Copyright (c) 2012 LexTang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios
+
+dependency 'JSONKit',      '~>1.4'
+dependency 'AFNetworking', '~>0.9.1'
+dependency 'BlocksKit',    '~>1.0.6'
+dependency 'SSToolkit',    '~>0.1.2'
